9500d89e43f04d0229a260c8a53ecba0ea139b68
Team20 <pmw2125@columbia.edu>
2016-09-26 20:14:07 -0400

    robot doesn't behave

--- findObject.py
+++ findObject.py
@@ -2,36 +2,41 @@ from gopigo import *
 import math                                       
 import time                                       
                                                   
-US_MAX_DIST = 160                           
+US_MAX_DIST = 140                           
 US_PIN = 15                                       
-SPEED = 150
-TIME_INCREMENTS = 0.2
+ROTATE_SPEED = 50
+DRIVE_SPEED = 140
+TIME_INCREMENTS = 0.20
 WHEEL_CIRCUMFERENCE = 20.4                        
-ENCODER_PPR = 64 # Pulses Per Revolution          
+ENCODER_PPR = 18 # Pulses Per Revolution          
 
 DIRECTION = True #True = Left, False = right      
-APPROACHING_DIST = 20                             
+APPROACHING_DIST = 22                             
                                                   
-def findObject(direction):                        
-    turnToObjectDirection(direction)              
-    approachObject(APPROACHING_DIST)
+#def findObject(direction):                        
+#    turnToObjectDirection(direction)              
+#    approachObject(APPROACHING_DIST)
 def approachObject(minDist):            
-    while(us_dist(US_PIN)-minDist>0):             
+    #while(us_dist(US_PIN)-minDist>0):             
+    while(True):
         if (isObject()):
             print 'I am approaching by steps:'
             stepSize = int((us_dist(US_PIN)-minDist)/2)
-            print stepSize
-            goForward(stepSize/2)
+            print 'step Size:' + str(stepSize)
+            print 'us_dist before goFwd()' + str(us_dist(US_PIN))
+            goForward(stepSize)
+            print 'us_dist after goFwd()' + str(us_dist(US_PIN))
             if (us_dist(US_PIN)-minDist < 0):
                 stop()
                 return
-            time.sleep(1)                       
+            time.sleep(.2)                       
         else:
             print 'lost target - recalibrate'
             direction = getObjDirection()
             if (direction == 99):
                 turnToObjectDirection(DIRECTION) 
-            turnToObjectDirection(direction)      
+            else:
+                turnToObjectDirection(direction)      
                                                   
 def getObjDirection():                            
     for i in range(20, 160,20 ):
@@ -49,19 +54,19 @@ def getObjDirection():
 def turnToObjectDirection(direction):             
     servo(90)
     while(not isObject()):
-        print 'object not found'
-        turn(direction,TIME_INCREMENTS,SPEED)
-    print 'object found'
+        print 'TurnToOBject(): object not found'
+        turn(direction,TIME_INCREMENTS,ROTATE_SPEED)
+    print 'TurnToObject(): object found'
     objectSize = 1                                
     while(isObject()):                              
         objectSize += 1                           
         print 'measuring object width'
-        turn(direction,TIME_INCREMENTS,SPEED)     
-    turn(not direction,objectSize/2*TIME_INCREMENTS,SPEED)                                          
+        turn(direction,TIME_INCREMENTS,ROTATE_SPEED)     
+    print 'object size:' + str(objectSize)
+    turn(not direction,objectSize/2*TIME_INCREMENTS,ROTATE_SPEED)                                          
 def isObject():
-    time.sleep(1)
+    time.sleep(.5)
     dist = us_dist(US_PIN)
-    print dist
     if (dist < US_MAX_DIST and dist > 5):           
         return 1                                  
     return 0                                      
@@ -73,11 +78,16 @@ def turn(direction, seconds, speed):
         right_rot()                               
     time.sleep(seconds)                           
     stop()                                        
-def goForward(distance):                          
-    distToWheelRatio = float(distance/WHEEL_CIRCUMFERENCE)
+def goForward(distance):
+    set_speed(DRIVE_SPEED)
+    distToWheelRatio = float(abs(distance)/WHEEL_CIRCUMFERENCE)
     encoder_counts = int(distToWheelRatio*ENCODER_PPR) 
-    enc_tgt(1,1,encoder_counts)                       
-    fwd()                                         
+    enc_tgt(1,1,encoder_counts) 
+    if (distance >= 0):
+        fwd() 
+    else:
+        bwd()
 if __name__=='__main__': 
     enable_servo()
-    findObject(DIRECTION)
+    approachObject(APPROACHING_DIST)
+    stop()
