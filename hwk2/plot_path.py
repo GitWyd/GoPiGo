cf33dc65fc1c6a24f5ddf796932540ec747a8a00
Varun Shetty <varun.shetty.1121@gmail.com>
2016-10-10 23:21:33 -0400

    Graph plots added for location and obstacle mapping

---
+++ hwk2/plot_path.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import follow_obstacle
+
+#location_history = [[1,1],[2,2],[5,5]]
+location_history = follow_obstacle.get_location_history()
+#obstacle_history = [[1,2],[1,3],[2,4],[4, 4]]
+obstacle_history follow_obstacle.get_obstacle_history()
+plt.xlim(0, 10)
+plt.ylim(0, 10)
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+ax = plt.axes()
+
+# Plotting the location along the m-line
+for i in range(1,len(location_history),1):
+	prev = location_history[i-1]
+	curr = location_history[i]
+	print (prev[0], prev[1], curr[0], curr[1])
+	ax.annotate("", xy=(curr[0], curr[1]), xytext=(prev[0], prev[1]),
+    arrowprops=dict(arrowstyle="->"))
+
+# Plotting the location along the obstacle
+for i in range(1,len(obstacle_history),1):
+	prev = obstacle_history[i-1]
+	curr = obstacle_history[i]
+	print (prev[0], prev[1], curr[0], curr[1])
+	ax.annotate("", xy=(curr[0], curr[1]), xytext=(prev[0], prev[1]),
+    arrowprops=dict(arrowstyle="->"))
+plt.show()
