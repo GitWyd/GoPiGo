90d5e3e254c6fcf94b9b0cae55985ff2f95f6d0d
GitWyd <pmw2125@columbia.edu>
2016-11-28 15:22:16 -0500

    Implemented bubble sort for vertcies

--- hwk5/obstacle.py
+++ hwk5/obstacle.py
@@ -4,18 +4,31 @@
 ##
 
 class Obstacle:
-        def __init__(self):
+        def __init__(self, vertices = None):
                 self.vertices = []
+                # add vetcies from list of tuples
+                if vertices:
+                        for pt in vertices:
+                                self.vertices.apend(Point(pt[0],pt[1]))
                 self.center = None
                 self.convex_hull = None
         def add_vertex(self, x, y):
                 pt = Point(x,y)
                 self.vertices.append(pt)
+                self._find_centroid()
+        def _find_centroid():
+                tot = sum(self.vertices)
+                n = len(self.vertices)
+                return Point(tot.x/n, tot.y/n)
         def _order_vertices_CW(self):
-                for i in range(vertices.len()-1):
-                        if det == 0:
-                                dist1 = dist(vertices[i], center)
-                                dist2 = dist(vertices[i+1], center)
+        # bubble sort algorithm ()
+                list = self.vertices 
+                for passnr in range(len(list)-1,0,-1):
+                        for i in range(passnr):
+                                if list[i].is_left_CW(list[i+1]):
+                                        temp = list[i]
+                                        list[i] = list[i+1]
+                                        list[i+1] = temp
         def convex_hull(self):
                 self.convex_hull = hull
 class Point:
@@ -24,7 +37,7 @@ class Point:
                 self.y = y
         # returns true if a point A is left of point B
         # in Clockwise ordering around the cetner
-        def is_left_CW(self, pt, center):
+        def is_left_CW(self, pt, center=self.center):
                 a = self
                 b = pt
                 c = center
@@ -45,6 +58,8 @@ class Point:
                 dist1 = (a.x-c.x)**2 + (a.y-c.y)**2
                 dist2 = (b.x-c.x)**2 + (b.y-c.y)**2
                 return dist1 > dist2
+        def __radd__(self, other):
+                return Point(self.x+other.x,self.y+other.y)
         def set_x(self, x):
                 self.x = x
         def set_y(self, y):
